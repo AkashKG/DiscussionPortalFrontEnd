c3d5c6dc89964d58e83aa232d98367ebd7373ffb
Akash Gupta <yesitsakash@gmail.com>
2017-07-23 08:57:49 +0530

    [DiscussionPortal][Front End][Initial Config] 1. Used Angular CLI 2. Added Angular Material 2 3. Proxy with backend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AppService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private _appService:AppService){}
+    app = [];
+    ngOnInit() {
+        this._appService.getEmployees()
+            .subscribe(resAppData => this.app = resAppData);
+
+        
+    }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AppService {
+    constructor(private _http:Http) {}
+    getEmployees() {
+        return this._http.get("/Project/answers/HiHello")
+                .map((response:Response) => response.json());
+    }
+}
